2ad20d20fe9eb4d1316d3288513fa25de95e8378
Anant Kanchan <anant4265@gmail.com>
2021-06-18 20:41:32 +0530

    :tada: initial commit

---
+++ CryptoX/CryptoXApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoXApp.swift
+//  CryptoX
+//
+//  Created by Anant Kanchan on 18/06/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

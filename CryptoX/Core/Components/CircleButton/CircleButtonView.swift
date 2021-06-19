e1fa15cbc6403b1a981cb6b7a35919fba0fcd677
Anant Kanchan <anant4265@gmail.com>
2021-06-19 17:32:38 +0530

    :package: code improved

--- CryptoX/Core/Components/CircleButton/CircleButtonView.swift
+++ CryptoX/Core/Components/CircleButton/CircleButtonView.swift
@@ -34,7 +34,7 @@ struct CircleButtonView_Previews: PreviewProvider {
             
             CircleButtonView(iconName: "plus")
                 .previewLayout(.sizeThatFits)
-                .colorScheme(.dark)
+                .preferredColorScheme(.dark)
         }
         
     }

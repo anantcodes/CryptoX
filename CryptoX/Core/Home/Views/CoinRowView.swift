27cbe2c49d678f76befae47778a63327f2c8e25d
Anant Kanchan <anant4265@gmail.com>
2021-06-19 16:42:48 +0530

    :art: coin row view ui

--- CryptoX/Core/Home/Views/CoinRowView.swift
+++ CryptoX/Core/Home/Views/CoinRowView.swift
@@ -12,7 +12,30 @@ struct CoinRowView: View {
     let coin: CoinModel
     
     var body: some View {
-        Text(coin.name)
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundColor(Color.theme.SecondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundColor(Color.theme.accent)
+            Spacer()
+            VStack(alignment: .trailing) {
+                Text("\(coin.currentPrice)")
+                    .bold()
+                    .foregroundColor(Color.theme.accent)
+                Text("\(coin.priceChangePercentage24H ?? 0)% ")
+                    .foregroundColor(
+                        (coin.priceChangePercentage24H ?? 0) >= 0 ?
+                        Color.theme.green :
+                            Color.theme.red
+                    )
+            }
+        }
     }
 }
 

403f8b77f4a53c15d6a855f8e077f0de41735707
Anant Kanchan <anant4265@gmail.com>
2021-06-19 13:18:35 +0530

    :sparkles: icons positioned

--- CryptoX/Core/Home/Views/HomeView.swift
+++ CryptoX/Core/Home/Views/HomeView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
     var body: some View {
         ZStack {
             // background layer
@@ -16,7 +19,23 @@ struct HomeView: View {
             
             //content layer
             VStack {
-                Text("Header")
+                HStack {
+                    CircleButtonView(iconName: "info")
+                    Spacer()
+                    Text("Live Prices")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.theme.accent)
+                    Spacer()
+                    CircleButtonView(iconName: "chevron.right")
+                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                        .onTapGesture {
+                            withAnimation(.spring()){
+                                showPortfolio.toggle()
+                            }
+                        }
+                }
+                .padding(.horizontal)
                 Spacer(minLength: 0)
             }
         }

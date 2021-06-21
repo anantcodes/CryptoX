3f443a31d0d3a6425bd40a00c16b5080d0b53916
Anant Kanchan <anant4265@gmail.com>
2021-06-21 20:39:45 +0530

    :fire: network error added

--- CryptoX/Utilities/NetworkingManager.swift
+++ CryptoX/Utilities/NetworkingManager.swift
@@ -11,12 +11,12 @@ import Combine
 class NetworkManager {
     
     enum NetworkingError: LocalizedError {
-        case badURLResponse
+        case badURLResponse(url: URL)
         case unknown
         
         var errorDescription: String? {
             switch self {
-            case .badURLResponse: return "[🔥] Bad response from URL."
+            case .badURLResponse(url: let url): return "[🔥] Bad response from URL: \(url)"
             case .unknown: return "[⚠️] Unknown error occured"
             }
         }
@@ -25,16 +25,17 @@ class NetworkManager {
     static func download(url: URL) -> AnyPublisher<Data, Error> {
       return URLSession.shared.dataTaskPublisher(for: url)
            .subscribe(on: DispatchQueue.global(qos: .default))
-           .tryMap({try handleURLResponse(output: $0)})
+           .tryMap({try handleURLResponse(output: $0, url: url)})
            .receive(on: DispatchQueue.main)
            .eraseToAnyPublisher()
     }
     
     
-    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        throw NetworkingError.badURLResponse(url: url)
         guard let response = output.response as? HTTPURLResponse,
               response.statusCode >= 200 && response.statusCode < 300 else {
-            throw NetworkingError.badURLResponse
+            throw NetworkingError.badURLResponse(url: url)
         }
         return output.data
     }

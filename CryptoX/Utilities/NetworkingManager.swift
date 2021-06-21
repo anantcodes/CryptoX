1831723fff2734374f5315078ff3cb5765e0c6d7
Anant Kanchan <anant4265@gmail.com>
2021-06-21 20:27:39 +0530

    :package: handling url response

--- CryptoX/Utilities/NetworkingManager.swift
+++ CryptoX/Utilities/NetworkingManager.swift
@@ -13,15 +13,18 @@ class NetworkManager {
     static func download(url: URL) -> AnyPublisher<Data, Error> {
       return URLSession.shared.dataTaskPublisher(for: url)
            .subscribe(on: DispatchQueue.global(qos: .default))
-           .tryMap { (output) -> Data in
-               guard let response = output.response as? HTTPURLResponse,
-                     response.statusCode >= 200 && response.statusCode < 300 else {
-                   throw URLError(.badServerResponse)
-               }
-               return output.data
-           }
+           .tryMap({try handleURLResponse(output: $0)})
            .receive(on: DispatchQueue.main)
            .eraseToAnyPublisher()
+    }
+    
+    
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            throw URLError(.badServerResponse)
+        }
+        return output.data
     }
     
     static func handleCompletion(completion: Subscribers.Completion<Error>) {

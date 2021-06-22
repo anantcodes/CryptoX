acd86f8ab77b2882d76cd5aeffc7df39563c4e47
Anant Kanchan <anant4265@gmail.com>
2021-06-22 19:07:08 +0530

    :sparkles: search bar setup in home view

--- CryptoX/Core/Home/Views/HomeView.swift
+++ CryptoX/Core/Home/Views/HomeView.swift
@@ -22,6 +22,8 @@ struct HomeView: View {
             VStack {
                 homeHeader
                 
+                SearchBarView(searchText: $vm.searchText)
+                
                 columnTitles
                 
                 if !showPortfolio {

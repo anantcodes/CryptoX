4b92def6ec456f8fda9a0497a952682fd2b9473a
Anant Kanchan <anant4265@gmail.com>
2021-06-22 12:54:16 +0530

    :sparkles: changed to progress

--- CryptoX/Core/Components/CoinImage/CoinImageView.swift
+++ CryptoX/Core/Components/CoinImage/CoinImageView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 class CoinImageViewModel: ObservableObject {
     
     @Published var image: UIImage? = nil
-    @Published var isLoading: Bool = false
+    @Published var isLoading: Bool = true
     
     init() {
         getImage()

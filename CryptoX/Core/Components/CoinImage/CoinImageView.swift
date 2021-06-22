2b79d4df3a2dee1ea2469e8117c4e8c09a061b58
Anant Kanchan <anant4265@gmail.com>
2021-06-22 12:53:34 +0530

    :sparkles: qsmrk added

--- CryptoX/Core/Components/CoinImage/CoinImageView.swift
+++ CryptoX/Core/Components/CoinImage/CoinImageView.swift
@@ -7,14 +7,45 @@
 
 import SwiftUI
 
+class CoinImageViewModel: ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    @Published var isLoading: Bool = false
+    
+    init() {
+        getImage()
+    }
+    
+    private func getImage() {
+        
+    }
+    
+}
+
 struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel = CoinImageViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundColor(Color.theme.SecondaryText)
+            }
+        }
     }
 }
 
 struct CoinImageView_Previews: PreviewProvider {
     static var previews: some View {
         CoinImageView()
+            .padding()
+            .previewLayout(.sizeThatFits)
     }
 }

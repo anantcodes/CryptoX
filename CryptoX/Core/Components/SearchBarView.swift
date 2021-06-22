fefca6037f640750e97653d61e96b0f747645bf6
Anant Kanchan <anant4265@gmail.com>
2021-06-22 15:28:03 +0530

    :lipstick: magnify glass added

--- CryptoX/Core/Components/SearchBarView.swift
+++ CryptoX/Core/Components/SearchBarView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct SearchBarView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: "magnifyingglass")
+        }
     }
 }
 

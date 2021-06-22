89af94852a895354370090e1a6be39cefe0b53f6
Anant Kanchan <anant4265@gmail.com>
2021-06-22 14:31:30 +0530

    :package: bitcoin img added for rows

--- CryptoX/Core/Home/Views/CoinRowView.swift
+++ CryptoX/Core/Home/Views/CoinRowView.swift
@@ -46,7 +46,7 @@ extension CoinRowView {
                 .font(.caption)
                 .foregroundColor(Color.theme.SecondaryText)
                 .frame(minWidth: 30)
-            Circle()
+           CoinImageView(coin: coin)
                 .frame(width: 30, height: 30)
             Text(coin.symbol.uppercased())
                 .font(.headline)

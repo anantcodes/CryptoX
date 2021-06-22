8bafad88533c53711d73a7fa8846f1d86d3fb6bc
Anant Kanchan <anant4265@gmail.com>
2021-06-22 13:18:23 +0530

    :sparkles: coimg service code

--- CryptoX/Services/CoinImageService.swift
+++ CryptoX/Services/CoinImageService.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageService {
+    
+    @Published var image: UIImage? = nil
+    
+    var imageSubscription: AnyCancellable?
+    
+    init(urlString: String) {
+        getCoinImage(urlstring: urlString)
+    }
+    
+    private func getCoinImage(urlstring: String) {
+        guard let url = URL(string: urlstring) else { return }
+        
+        imageSubscription = NetworkManager.download(url: url)
+            .tryMap({ (data) -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+                self?.imageSubscription?.cancel()
+            })
+    }
+    
+    
+    
+}

01533323d989c5110461f4e40c7c1afbd909288f
Anant Kanchan <anant4265@gmail.com>
2021-06-23 15:08:55 +0530

    :sparkles: debounce enabled

--- CryptoX/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoX/Core/Home/ViewModels/HomeViewModel.swift
@@ -27,6 +27,7 @@ class HomeViewModel: ObservableObject {
         // updates allcoins
         $searchText
             .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
             .map(filterCoins)
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins

7624b9f878404ac3acb1badb0eb5ece1347ce204
Anant Kanchan <anant4265@gmail.com>
2021-06-23 14:36:54 +0530

    :tada: searching map coded

--- CryptoX/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoX/Core/Home/ViewModels/HomeViewModel.swift
@@ -23,11 +23,27 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        
         dataService.$allCoins
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+        
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .map { (text, startingCoins) -> [CoinModel] in
+                
+                guard !text.isEmpty else {
+                    return startingCoins
+                }
+                
+                let lowercasedText = text.lowercased()
+                
+                return startingCoins.filter { (coin) -> Bool in
+                    return coin.name.lowercased().contains(lowercasedText) ||
+                        coin.symbol.lowercased().contains(lowercasedText) ||
+                        coin.id.lowercased().contains(lowercasedText)
+                }
+            }
     }
 }

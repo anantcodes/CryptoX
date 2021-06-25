39104212f97dd33e9d2eadfb7710e03902bd2d90
Anant Kanchan <anant4265@gmail.com>
2021-06-25 10:12:01 +0530

    :tada: statistic model

---
+++ CryptoX/Models/StatisticModel.swift
@@ -0,0 +1,24 @@
+//
+//  StatisticModel.swift
+//  CryptoX
+//
+//  Created by Anant Kanchan on 25/06/21.
+//
+
+import Foundation
+
+struct StatisticModel: Identifiable {
+    let id = UUID().uuidString
+    let title: String
+    let value: String
+    let percentageChange: Double?
+    
+    init(title: String, value: String, percentageChange: Double? = nil) {
+        self.title = title
+        self.value = value
+        self.percentageChange = percentageChange
+    }
+}
+
+
+let newModel = StatisticModel(title: "", value: "", percentageChange: nil)

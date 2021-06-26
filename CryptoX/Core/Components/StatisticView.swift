0527c26300da301707c78471b8854ac82b1f1e1a
Anant Kanchan <anant4265@gmail.com>
2021-06-26 16:32:25 +0530

    :tada: statistic view

---
+++ CryptoX/Core/Components/StatisticView.swift
@@ -0,0 +1,20 @@
+//
+//  StatisticView.swift
+//  CryptoX
+//
+//  Created by Anant Kanchan on 26/06/21.
+//
+
+import SwiftUI
+
+struct StatisticView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StatisticView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatisticView()
+    }
+}

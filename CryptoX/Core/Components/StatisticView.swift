c79312527f36606f7eb9a999d39c43ec5932ea6e
Anant Kanchan <anant4265@gmail.com>
2021-06-26 16:39:44 +0530

    :sparkles: work on statistic view

--- CryptoX/Core/Components/StatisticView.swift
+++ CryptoX/Core/Components/StatisticView.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct StatisticView: View {
+    
+    let stat: StatisticModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(stat.title)
     }
 }
 
 struct StatisticView_Previews: PreviewProvider {
     static var previews: some View {
-        StatisticView()
+        StatisticView(stat: dev.stat1)
     }
 }
--- CryptoX/Extensions/PreviewProvider.swift
+++ CryptoX/Extensions/PreviewProvider.swift
@@ -24,6 +24,9 @@ class DeveloperPreview {
     
     let homeVM = HomeViewModel()
     
+    let stat1 = StatisticModel(title: "Market Cap", value: "$12.5Bn", percentageChange: 25.34)
+    let stat2 = StatisticModel(title: "Total Volume", value: "$1.23Tr")
+    
     let coin = CoinModel(
         id: "bitcoin",
               symbol: "btc",

70d1974ffa95262de6ebf3572eaf2b2795ae5449
Anant Kanchan <anant4265@gmail.com>
2021-06-29 19:21:57 +0530

    :tada: home stats view

---
+++ CryptoX/Core/Home/Views/HomeStatsView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeStatsView.swift
+//  CryptoX
+//
+//  Created by Anant Kanchan on 29/06/21.
+//
+
+import SwiftUI
+
+struct HomeStatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeStatsView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeStatsView()
+    }
+}

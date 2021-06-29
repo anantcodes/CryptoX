dd75e21e8c7805f5596c127c07ed8c74cc636496
Anant Kanchan <anant4265@gmail.com>
2021-06-29 19:24:27 +0530

    :sparkles: work on statistic model

--- CryptoX/Core/Home/Views/HomeStatsView.swift
+++ CryptoX/Core/Home/Views/HomeStatsView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct HomeStatsView: View {
+    
+    let statistics: [StatisticModel] = [
+        StatisticModel(title: "Title", value: "Value", percentageChange: 1),
+        StatisticModel(title: "Title", value: "Value"),
+        StatisticModel(title: "Title", value: "Value"),
+        StatisticModel(title: "Title", value: "Value", percentageChange: -7),
+    ]
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }

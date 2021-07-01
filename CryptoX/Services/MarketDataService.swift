d76fc4846e262d6a75d7b46d5e35854592887003
Anant Kanchan <anant4265@gmail.com>
2021-07-01 20:34:02 +0530

    :sparkles: market data service

---
+++ CryptoX/Services/MarketDataService.swift
@@ -0,0 +1,31 @@
+//
+//  MarketDataService.swift
+//  CryptoX
+//
+//  Created by Anant Kanchan on 01/07/21.
+//
+
+import Foundation
+import Combine
+class MarketDataService {
+    
+    @Published var marketData: MarketDataModel? = nil
+    var marketDataSubscription: AnyCancellable?
+    
+    init() {
+        getData()
+    }
+    
+    private func getData() {
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
+        
+        marketDataSubscription = NetworkManager.download(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] (returnedGlobalData) in
+                self?.marketData = returnedGlobalData.data
+                self?.marketDataSubscription?.cancel()
+            })
+    }
+}
+

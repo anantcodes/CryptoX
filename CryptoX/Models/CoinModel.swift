e26d9cc76c54f3c47a1a1cc662468f94058fe0c4
Anant Kanchan <anant4265@gmail.com>
2021-07-01 20:27:23 +0530

    :sparkles: market data model

--- CryptoX/Models/CoinModel.swift
+++ CryptoX/Models/CoinModel.swift
@@ -11,7 +11,7 @@ import Foundation
 /*
  
  
- https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h
+ https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h
  
  
  
--- CryptoX/Services/CoinDataService.swift
+++ CryptoX/Services/CoinDataService.swift
@@ -19,7 +19,7 @@ class CoinDataService {
     
     private func getCoins() {
         
-        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
         
         coinSubscription = NetworkManager.download(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())

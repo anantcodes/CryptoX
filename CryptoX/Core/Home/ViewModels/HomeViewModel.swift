a9beb0bd977f87de819371316fe21f66864f4a44
Anant Kanchan <anant4265@gmail.com>
2021-07-02 12:33:11 +0530

    :sparkles: market data added in model

--- CryptoX/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoX/Core/Home/ViewModels/HomeViewModel.swift
@@ -10,12 +10,7 @@ import Combine
 
 class HomeViewModel: ObservableObject {
     
-    @Published var statistics: [StatisticModel] = [
-        StatisticModel(title: "Title", value: "Value", percentageChange: 1),
-        StatisticModel(title: "Title", value: "Value"),
-        StatisticModel(title: "Title", value: "Value"),
-        StatisticModel(title: "Title", value: "Value", percentageChange: -7),
-    ]
+    @Published var statistics: [StatisticModel] = []
     
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
@@ -23,6 +18,7 @@ class HomeViewModel: ObservableObject {
     @Published var searchText: String = ""
     
     private let coinDataService = CoinDataService()
+    private let marketDataService = MarketDataService()
     private var cancellables = Set<AnyCancellable>()
     
     init() {
@@ -40,6 +36,36 @@ class HomeViewModel: ObservableObject {
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+        
+        
+        
+        marketDataService.$marketData
+            .map { (marketDataModel) -> [StatisticModel] in
+                var stats: [StatisticModel] = []
+
+                guard let data = marketDataModel else {
+                    return stats
+                }
+                
+                let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+                let volume = StatisticModel(title: "24h Volume", value: data.volume)
+                let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+                let portfolio = StatisticModel(title: "Portfolio Value", value: "$0.00", percentageChange: 0)
+                
+                stats.append(contentsOf: [
+                    marketCap,
+                    volume,
+                    btcDominance,
+                    portfolio
+                ])
+                return stats
+            }
+            .sink { [weak self] (returnedStats) in
+                self?.statistics = returnedStats
+            }
+            .store(in: &cancellables)
+        
+        
     }
     
     private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {

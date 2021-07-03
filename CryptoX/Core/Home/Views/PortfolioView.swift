2bf94df260f24c1fa0cbfd727aa74eefc226cc63
Anant Kanchan <anant4265@gmail.com>
2021-07-03 12:56:06 +0530

    :tada: portfolio view

---
+++ CryptoX/Core/Home/Views/PortfolioView.swift
@@ -0,0 +1,20 @@
+//
+//  PortfolioView.swift
+//  CryptoX
+//
+//  Created by Anant Kanchan on 03/07/21.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PortfolioView_Previews: PreviewProvider {
+    static var previews: some View {
+        PortfolioView()
+    }
+}

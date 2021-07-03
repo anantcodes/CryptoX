0fcc22b1e3a143bd5c1c14c6abb3717ecc8ada88
Anant Kanchan <anant4265@gmail.com>
2021-07-03 14:19:27 +0530

    :sparkles: coin logo ui

--- CryptoX/Core/Components/CoinImage/CoinLogoView.swift
+++ CryptoX/Core/Components/CoinImage/CoinLogoView.swift
@@ -14,12 +14,32 @@ struct CoinLogoView: View {
     var body: some View {
         VStack {
             CoinImageView(coin: coin)
+                .frame(width: 50, height: 50)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundColor(Color.theme.accent)
+                .lineLimit(1)
+                .minimumScaleFactor(0.5)
+            Text(coin.name)
+                .font(.caption)
+                .foregroundColor(Color.theme.SecondaryText)
+                .lineLimit(2)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
         }
     }
 }
 
 struct CoinLogoView_Previews: PreviewProvider {
     static var previews: some View {
-        CoinLogoView(coin: dev.coin)
+        Group {
+            CoinLogoView(coin: dev.coin)
+                .previewLayout(.sizeThatFits)
+            
+            CoinLogoView(coin: dev.coin)
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+        }
+        
     }
 }

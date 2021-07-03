dab7b4fcd0f8ac4c514c3dfdc79e883a08aaba99
Anant Kanchan <anant4265@gmail.com>
2021-07-03 13:17:23 +0530

    :tada: xmark btn

---
+++ CryptoX/Core/Components/XMarkButton.swift
@@ -0,0 +1,20 @@
+//
+//  XMarkButton.swift
+//  CryptoX
+//
+//  Created by Anant Kanchan on 03/07/21.
+//
+
+import SwiftUI
+
+struct XMarkButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct XMarkButton_Previews: PreviewProvider {
+    static var previews: some View {
+        XMarkButton()
+    }
+}

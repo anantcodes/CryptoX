68f92d00f18ab81c2de1df84018b2756cc6afaeb
Anant Kanchan <anant4265@gmail.com>
2021-07-03 14:10:29 +0530

    :tada: coin logo view

---
+++ CryptoX/Core/Components/CoinImage/CoinLogoView.swift
@@ -0,0 +1,20 @@
+//
+//  CoinLogoView.swift
+//  CryptoX
+//
+//  Created by Anant Kanchan on 03/07/21.
+//
+
+import SwiftUI
+
+struct CoinLogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoinLogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinLogoView()
+    }
+}

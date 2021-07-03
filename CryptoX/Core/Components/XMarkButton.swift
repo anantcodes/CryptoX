93b315af82ce336b4d1a9540a07be0dd3d9710b8
Anant Kanchan <anant4265@gmail.com>
2021-07-03 13:18:52 +0530

    :construction: xmark

--- CryptoX/Core/Components/XMarkButton.swift
+++ CryptoX/Core/Components/XMarkButton.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct XMarkButton: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            presentationMode.wrappedValue.dismiss()
+        }, label: {
+            Image(systemName: "xmark")
+                .font(.headline)
+        })
     }
 }
 

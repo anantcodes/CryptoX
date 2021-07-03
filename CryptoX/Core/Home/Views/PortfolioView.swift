3f70855bf21841d15ccdd836bc0f4d5ba919be24
Anant Kanchan <anant4265@gmail.com>
2021-07-03 14:36:56 +0530

    :hammer: scroll view extension

--- CryptoX/Core/Home/Views/PortfolioView.swift
+++ CryptoX/Core/Home/Views/PortfolioView.swift
@@ -18,28 +18,9 @@ struct PortfolioView: View {
                 VStack(alignment: .leading, spacing: 0) {
                     SearchBarView(searchText: $vm.searchText)
                     
-                    ScrollView(.horizontal, showsIndicators: false, content: {
-                        LazyHStack(spacing: 10) {
-                            ForEach(vm.allCoins) { coin in
-                                CoinLogoView(coin: coin)
-                                    .frame(width: 75)
-                                    .padding(4)
-                                    .onTapGesture {
-                                        withAnimation(.easeIn) {
-                                            selectedCoin = coin
-                                        }
-                                    }
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(selectedCoin?.id == coin.id ? Color.theme.green : Color.clear
-                                                    , lineWidth: 1)
-                                            
-                                    )
-                            }
-                        }
-                        .padding(.vertical, 4)
-                        .padding(.leading)
-                    })
+                    coinLogoList
+                    
+                    
                 }
             }
             .navigationTitle("Edit Portfolio")
@@ -56,5 +37,34 @@ struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
         PortfolioView()
             .environmentObject(dev.homeVM)
+    }
+}
+
+
+extension PortfolioView {
+    
+    private var coinLogoList: some View {
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            LazyHStack(spacing: 10) {
+                ForEach(vm.allCoins) { coin in
+                    CoinLogoView(coin: coin)
+                        .frame(width: 75)
+                        .padding(4)
+                        .onTapGesture {
+                            withAnimation(.easeIn) {
+                                selectedCoin = coin
+                            }
+                        }
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(selectedCoin?.id == coin.id ? Color.theme.green : Color.clear
+                                        , lineWidth: 1)
+                                
+                        )
+                }
+            }
+            .padding(.vertical, 4)
+            .padding(.leading)
+        })
     }
 }

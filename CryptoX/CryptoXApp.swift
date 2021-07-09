c62abaa0ea0b72fc90fc09234d8ac14c60184d05
Anant Kanchan <anant4265@gmail.com>
2021-07-09 15:54:10 +0530

    :lipstick: init appearance

--- CryptoX/CryptoXApp.swift
+++ CryptoX/CryptoXApp.swift
@@ -12,6 +12,11 @@ struct CryptoXApp: App {
     
     @StateObject private var vm = HomeViewModel()
     
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {

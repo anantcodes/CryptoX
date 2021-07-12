9cd07ca588d1d8919c7b0f4e5926daa2d673dd6f
Anant Kanchan <anant4265@gmail.com>
2021-07-12 14:55:58 +0530

    :tada: portfolio core data work

---
+++ CryptoX/Services/PortfolioDataService.swift
@@ -0,0 +1,39 @@
+//
+//  PortfolioDataService.swift
+//  CryptoX
+//
+//  Created by Anant Kanchan on 12/07/21.
+//
+
+import Foundation
+import CoreData
+
+class PortfolioDataService {
+    
+    private let container: NSPersistentContainer
+    private let containerName: String = "PortfolioContainer"
+    private let entityName: String = "PortfolioEntity"
+    
+    @Published var savedEntities: [PortfolioEntity] = []
+    
+    
+    init() {
+        container = NSPersistentContainer(name: containerName)
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                print("Error loading Core Data! \(error)")
+            }
+        }
+    }
+    
+    
+    private func getPortfolio() {
+        let request = NSFetchRequest<PortfolioEntity>(entityName: entityName)
+        do {
+            savedEntities = try container.viewContext.fetch(request)
+        } catch let error {
+            print("Error fetching Portfolio Entities. \(error)")
+        }
+    }
+    
+}

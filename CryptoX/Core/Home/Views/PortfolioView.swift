cd9a9d31dac7f3308ecc00d64c5fb283fa50e216
Anant Kanchan <anant4265@gmail.com>
2021-07-12 14:39:04 +0530

    :bug: xmark bug fixed

--- CryptoX/Core/Home/Views/PortfolioView.swift
+++ CryptoX/Core/Home/Views/PortfolioView.swift
@@ -36,6 +36,11 @@ struct PortfolioView: View {
                     trailingNavBarButtons
                 }
             })
+            .onChange(of: vm.searchText, perform: { value in
+                if value == "" {
+                    removeSelectedCoin()
+                }
+            })
         }
     }
 }

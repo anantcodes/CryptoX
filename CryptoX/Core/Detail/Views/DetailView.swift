24b2a56df23c620f495911bce4dfe46f630eddf4
Anant Kanchan <anant4265@gmail.com>
2021-07-15 19:53:38 +0530

    :hammer: detail view optimised for faster runtime

--- CryptoX/Core/Detail/Views/DetailView.swift
+++ CryptoX/Core/Detail/Views/DetailView.swift
@@ -7,9 +7,24 @@
 
 import SwiftUI
 
+struct DetailLoadingView: View {
+    
+    @Binding var coin: CoinModel?
+    
+    var body: some View {
+        ZStack {
+            if let coin = coin {
+                DetailView(coin: coin)
+            }
+        }
+    }
+
+    
+}
+
 struct DetailView: View {
     
-    let coin: CoinModel
+     let coin: CoinModel
     
     init(coin: CoinModel) {
         self.coin = coin
--- CryptoX/Core/Home/Views/HomeView.swift
+++ CryptoX/Core/Home/Views/HomeView.swift
@@ -13,6 +13,9 @@ struct HomeView: View {
     @State private var showPortfolio: Bool = false // animate right
     @State private var showPortfolioView: Bool = false // new sheet
     
+    @State private var selectedCoin: CoinModel? = nil
+    @State private var showDetailView: Bool = false
+    
     var body: some View {
         ZStack {
             // background layer
@@ -42,6 +45,12 @@ struct HomeView: View {
                 Spacer(minLength: 0)
             }
         }
+        .background(
+            NavigationLink(
+                destination: DetailLoadingView(coin: $selectedCoin),
+                isActive: $showDetailView,
+                label: { EmptyView() })
+        )
     }
 }
 
@@ -90,13 +99,11 @@ extension HomeView {
     private var allCoinsList: some View {
         List {
             ForEach(vm.allCoins) { coin in
-                NavigationLink(
-                    destination: DetailView(coin: coin),
-                    label: {
-                        CoinRowView(coin: coin, showHoldingColumn: false)
-                            .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
-                    })
-                
+                CoinRowView(coin: coin, showHoldingColumn: false)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+                    .onTapGesture {
+                        segue(coin: coin)
+                    }
             }
         }
         .listStyle(PlainListStyle())
@@ -107,9 +114,17 @@ extension HomeView {
             ForEach(vm.portfolioCoins) { coin in
                 CoinRowView(coin: coin, showHoldingColumn: true)
                     .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+                    .onTapGesture {
+                        segue(coin: coin)
+                    }
             }
         }
         .listStyle(PlainListStyle())
+    }
+    
+    private func segue(coin: CoinModel) {
+        selectedCoin = coin
+        showDetailView.toggle()
     }
     
     private var columnTitles: some View {

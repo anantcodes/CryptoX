82f8074e380a4f50fc5e595c21e73a453ddbc489
Anant Kanchan <anant4265@gmail.com>
2021-07-15 13:48:07 +0530

    :tada: detail view

---
+++ CryptoX/Core/Detail/Views/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  CryptoX
+//
+//  Created by Anant Kanchan on 15/07/21.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}

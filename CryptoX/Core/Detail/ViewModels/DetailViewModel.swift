99f8a7c6cfb433889085ff0f526307e6fcc84591
Anant Kanchan <anant4265@gmail.com>
2021-07-16 17:34:20 +0530

    :sparkles: detail view model

--- CryptoX/Core/Detail/ViewModels/DetailViewModel.swift
+++ CryptoX/Core/Detail/ViewModels/DetailViewModel.swift
@@ -10,7 +10,10 @@ import Combine
 
 class DetailViewModel: ObservableObject {
     
-    let coin: CoinModel
+    @Published var overviewStatistics: [StatisticModel] = []
+    @Published var additionalStatistics: [StatisticModel] = []
+    
+    @Published var coin: CoinModel
     private let coinDetailService: CoinDetailDataService
     private var cancellables = Set<AnyCancellable>()
     
@@ -23,9 +26,59 @@ class DetailViewModel: ObservableObject {
     private func addSubscribers() {
         
         coinDetailService.$coinDetails
-            .sink { (returnedCoinDetails) in
-                print("RECEIVED COIN DETAIL DATA")
-                print(returnedCoinDetails)
+            .combineLatest($coin)
+            .map({ (coinDetailModel, coinModel) -> (overview: [StatisticModel], additional: [StatisticModel]) in
+                
+                // overview
+                let price = coinModel.currentPrice.asCurrencyWith6Decimals()
+                let pricePercentChange = coinModel.priceChangePercentage24H
+                let priceStat = StatisticModel(title: "Current Price", value: price, percentageChange: pricePercentChange)
+                
+                let marketCap = "$" + (coinModel.marketCap?.formattedWithAbbreviations() ?? "")
+                let marketCapPercentChange = coinModel.marketCapChangePercentage24H
+                let marketCapStat = StatisticModel(title: "Market Capitalization", value: marketCap, percentageChange: marketCapPercentChange)
+                
+                let rank = "\(coinModel.rank)"
+                let rankStat = StatisticModel(title: "Rank", value: rank)
+                
+                let volume = "$" + (coinModel.totalVolume?.formattedWithAbbreviations() ?? "")
+                let volumeStat = StatisticModel(title: "Volume", value: volume)
+                
+                let overviewArray: [StatisticModel] = [
+                    priceStat, marketCapStat, rankStat, volumeStat
+                ]
+                
+                // additional
+                let high = coinModel.high24H?.asCurrencyWith6Decimals() ?? "n/a"
+                let highStat = StatisticModel(title: "24h high", value: high)
+                
+                let low = coinModel.low24H?.asCurrencyWith6Decimals() ?? "n/a"
+                let lowStat = StatisticModel.init(title: "24h Low", value: low)
+                
+                let priceChange = coinModel.priceChange24H?.asCurrencyWith6Decimals() ?? "n/a"
+                let pricePercentChange2 = coinModel.priceChangePercentage24H
+                let priceChangeStat = StatisticModel(title: "24h Price Change", value: priceChange, percentageChange: pricePercentChange2)
+                
+                let marketCapChange = "$" + (coinModel.marketCapChange24H?.formattedWithAbbreviations() ?? "")
+                let marketCapPercentChange2 = coinModel.marketCapChangePercentage24H
+                let marketCapChangeStat = StatisticModel(title: "24h Market Cap Change", value: marketCapChange, percentageChange: marketCapPercentChange2)
+                
+                let blockTime = coinDetailModel?.blockTimeInMinutes ?? 0
+                let blockTimeString = blockTime == 0 ? "n/a" : "\(blockTime)"
+                let blockStat = StatisticModel(title: "Block Time", value: blockTimeString)
+                
+                let hashing = coinDetailModel?.hashingAlgorithm ?? "n/a"
+                let hashingStat = StatisticModel(title: "Hashing Algorithm", value: hashing)
+                
+                let additionalArray: [StatisticModel] = [
+                    highStat, lowStat, priceChangeStat, marketCapChangeStat, blockStat, hashingStat
+                ]
+                
+                return (overviewArray, additionalArray)
+            })
+            .sink { [weak self] (returnedArrays) in
+                self?.overviewStatistics = returnedArrays.overview
+                self?.additionalStatistics = returnedArrays.additional
             }
             .store(in: &cancellables)
         

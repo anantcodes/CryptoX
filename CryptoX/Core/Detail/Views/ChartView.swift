787dde1955b7075ce90a8e1e29e9c1174de4e59a
Anant Kanchan <anant4265@gmail.com>
2021-07-19 18:13:39 +0530

    :hammer: geometry reader added

--- CryptoX/Core/Detail/Views/ChartView.swift
+++ CryptoX/Core/Detail/Views/ChartView.swift
@@ -17,25 +17,28 @@ struct ChartView: View {
     
     
     var body: some View {
-        Path { path in
-            for index in data.indices {
-                
-                let xPosition = UIScreen.main.bounds.width / CGFloat(data.count) * CGFloat(index + 1)
-                
-                
-                if index == 0 {
-                    path.move(to: CGPoint(x: 0, y: 0))
+        GeometryReader { geometry in
+            Path { path in
+                for index in data.indices {
+                    
+                    let xPosition = geometry.size.width / CGFloat(data.count) * CGFloat(index + 1)
+                    
+                    
+                    if index == 0 {
+                        path.move(to: CGPoint(x: 0, y: 0))
+                    }
+                    path.addLine(to: CGPoint(x: xPosition, y: 0))
+                    
                 }
-                path.addLine(to: CGPoint(x: xPosition, y: 0))
-                
             }
+            .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
         }
-        .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
     }
 }
 
 struct ChartView_Previews: PreviewProvider {
     static var previews: some View {
         ChartView(coin: dev.coin)
+            .frame(width: 200)
     }
 }

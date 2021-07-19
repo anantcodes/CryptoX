0ac3eb2495e0fe0fb4e268a4442b1931e32df51d
Anant Kanchan <anant4265@gmail.com>
2021-07-19 17:34:13 +0530

    :tada: Chart view

---
+++ CryptoX/Core/Detail/Views/ChartView.swift
@@ -0,0 +1,20 @@
+//
+//  ChartView.swift
+//  CryptoX
+//
+//  Created by Anant Kanchan on 19/07/21.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView()
+    }
+}

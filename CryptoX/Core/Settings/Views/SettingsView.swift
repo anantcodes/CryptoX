876f5314be3d8f24641d0af9597de3190b574b42
Anant Kanchan <anant4265@gmail.com>
2021-07-20 17:31:09 +0530

    :sparkles: app info

--- CryptoX/Core/Settings/Views/SettingsView.swift
+++ CryptoX/Core/Settings/Views/SettingsView.swift
@@ -8,15 +8,33 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    let defaultURL = URL(string: "https://www.google.com")!
+    let githubURL = URL(string: "https://github.com/anantcodes")!
+    let linkedinURL = URL(string: "https://www.linkedin.com/in/anantkanchan/")!
+    let coingeckoURL = URL(string: "https://www.coingecko.com")!
+    let personalURL = URL(string: "https://github.com/anantcodes")!
+    
     var body: some View {
         NavigationView {
             List {
-                
-                Section(header: Text("header")) {
-                    Text("Hi")
-                    Text("Hi")
+                Section(header: Text("CryptoX")) {
+                    VStack(alignment: .leading) {
+                        Image("logo")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                        Text("This app was made by Anant Kanchan.It uses MVVM Architecture, Combine and CoreData!")
+                            .font(.callout)
+                            .fontWeight(.medium)
+                            .foregroundColor(Color.theme.accent)
+                    }
+                    .padding(.vertical)
+                    Link("GitHub", destination: githubURL)
                 }
             }
+            .font(.headline)
+            .accentColor(.blue)
             .listStyle(GroupedListStyle())
             .navigationTitle("Settings")
             .toolbar {

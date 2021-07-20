4596605634bb5f5be62f25af266fb37df7c39c66
Anant Kanchan <anant4265@gmail.com>
2021-07-20 17:04:43 +0530

    :tada: settingsview added

---
+++ CryptoX/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  CryptoX
+//
+//  Created by Anant Kanchan on 20/07/21.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}

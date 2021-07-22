3ccd5b55ba5bcb72169d74425cd146344be93db0
Anant Kanchan <anant4265@gmail.com>
2021-07-22 18:43:44 +0530

    :lipstick: loading screen animation

--- CryptoX/Core/Launch/Views/LaunchView.swift
+++ CryptoX/Core/Launch/Views/LaunchView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct LaunchView: View {
+    
+    @State private var loadingText: String = "Loading your portfolio..."
+    @State private var showLoadingText: Bool = false
+    
     var body: some View {
         ZStack {
             Color.launch.background
@@ -17,6 +21,19 @@ struct LaunchView: View {
                 .resizable()
                 .frame(width: 100, height: 100)
             
+            ZStack {
+                if showLoadingText {
+                    Text(loadingText)
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.launch.accent)
+                        .transition(AnyTransition.scale.animation(.easeIn))
+                }
+            }
+            .offset(y: 70)
+        }
+        .onAppear {
+            showLoadingText.toggle()
         }
     }
 }

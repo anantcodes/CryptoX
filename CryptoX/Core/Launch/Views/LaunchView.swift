4ae34bc25d88eff64e6490f6856c981804725b4d
Anant Kanchan <anant4265@gmail.com>
2021-07-22 12:33:54 +0530

    :tada: launch view

---
+++ CryptoX/Core/Launch/Views/LaunchView.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchView.swift
+//  CryptoX
+//
+//  Created by Anant Kanchan on 22/07/21.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView()
+    }
+}

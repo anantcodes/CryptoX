0c4c8518e593496bffdabfd3f253920b399db931
Anant Kanchan <anant4265@gmail.com>
2021-07-22 18:53:25 +0530

    :lipstick: loading screen animation updated

--- CryptoX/Core/Launch/Views/LaunchView.swift
+++ CryptoX/Core/Launch/Views/LaunchView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct LaunchView: View {
     
-    @State private var loadingText: String = "Loading your portfolio..."
+    @State private var loadingText: [String] = "Loading your portfolio...".map { String($0) }
     @State private var showLoadingText: Bool = false
+    private let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
+    @State private var counter: Int = 0
     
     var body: some View {
         ZStack {
@@ -23,11 +26,16 @@ struct LaunchView: View {
             
             ZStack {
                 if showLoadingText {
-                    Text(loadingText)
-                        .font(.headline)
-                        .fontWeight(.heavy)
-                        .foregroundColor(Color.launch.accent)
-                        .transition(AnyTransition.scale.animation(.easeIn))
+                    HStack(spacing: 0) {
+                        ForEach(loadingText.indices) { index in
+                            Text(loadingText[index])
+                                .font(.headline)
+                                .fontWeight(.heavy)
+                                .foregroundColor(Color.launch.accent)
+                                .offset(y: counter == index ? -5 : 0)
+                        }
+                    }
+                    .transition(AnyTransition.scale.animation(.easeIn))
                 }
             }
             .offset(y: 70)
@@ -35,6 +43,11 @@ struct LaunchView: View {
         .onAppear {
             showLoadingText.toggle()
         }
+        .onReceive(timer, perform: { _ in
+            withAnimation(.spring()) {
+                counter += 1
+            }
+        })
     }
 }
 

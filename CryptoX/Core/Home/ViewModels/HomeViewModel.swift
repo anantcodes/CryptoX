064389b6726c15c262a8883bb3136b1a5c43b418
Anant Kanchan <anant4265@gmail.com>
2021-07-24 16:17:50 +0530

    :bug: portfolio percentage change value updated

--- CryptoX/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoX/Core/Home/ViewModels/HomeViewModel.swift
@@ -166,7 +166,7 @@ class HomeViewModel: ObservableObject {
             }
             .reduce(0, +)
         
-        let percentageChange = ((portfolioValue - previousValue) / previousValue) * 100
+        let percentageChange = ((portfolioValue - previousValue) / previousValue)
         
         let portfolio = StatisticModel(
             title: "Portfolio Value",
